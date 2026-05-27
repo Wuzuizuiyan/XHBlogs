ecc45daab8f5c5676dbcb1a722933971d2bd6b4d
Wuzuizuiyan <Wuzuizuiyan@163.com>
2026-05-28 02:44:17 +0800

    fix: 更新友链申请格式

--- siteConfig.ts
+++ siteConfig.ts
@@ -106,7 +106,7 @@ export const siteConfig = {
   },
 
   // ── 15. 友链申请格式 ──
-  friendLinkApplyFormat: "名称：雾醉醉言\n简介：雾里醉语\n链接：https://www.wuzuizuiyan.cn\n头像：http://img.wuzuizuiyan.cn:21579/你的图片链接",
+  friendLinkApplyFormat: "名称：雾醉醉言\n简介：雾里醉语\n链接：https://www.wuzuizuiyan.cn\n头像：你的头像图片链接",
 
   // ── 16. 等级系统开关 ──
   enableLevelSystem: false,
