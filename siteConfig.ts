3b7c4f4eafbf78b50e6646e1d195f8e54abfb312
Wuzuizuiyan <Wuzuizuiyan@163.com>
2026-05-28 02:45:00 +0800

    fix: 友链头像使用实际 URL

--- siteConfig.ts
+++ siteConfig.ts
@@ -106,7 +106,7 @@ export const siteConfig = {
   },
 
   // ── 15. 友链申请格式 ──
-  friendLinkApplyFormat: "名称：雾醉醉言\n简介：雾里醉语\n链接：https://www.wuzuizuiyan.cn\n头像：你的头像图片链接",
+  friendLinkApplyFormat: "名称：雾醉醉言\n简介：雾里醉语\n链接：https://www.wuzuizuiyan.cn\n头像：https://s41.ax1x.com/2026/05/28/pmiQzJf.jpg",
 
   // ── 16. 等级系统开关 ──
   enableLevelSystem: false,
